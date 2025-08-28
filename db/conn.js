40eb0142b8eda8760c34c0a8e53da6b6d6ce95ad
PEDRO RAFAEL SANTIAGO <pedro_r_santiago@estudante.sesisenai.org.br>
2025-08-28 09:25:51 -0300

    trabalho

---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+
+    host: process.env.DB_HOST,
+
+    dialect: 'mysql',
+
+    port: process.env.DB_PORT
+    
+})
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('Conexão com o banco de dados realizada com sucesso.')
+})
+.catch((err)=>{
+    console.error('Não foi possível se a conexão com o banco de dados: ', err)
+})
+
+module.exports = sequelize
